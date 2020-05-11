c63c4f31f63c40da40d973371a9ae99d80dc12dc
lucasmpaim <lucasmpaim1@gmail.com>
2020-05-11 10:37:41 -0300

    fix(core): fix spm distribution

--- Package.swift
+++ Package.swift
@@ -21,7 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NetAnalyser",
-            dependencies: []),
+            dependencies: [
+            	"SnapKit", "DateHelper"
+            ]),
         .testTarget(
             name: "NetAnalyserTests",
             dependencies: ["NetAnalyser"]),
