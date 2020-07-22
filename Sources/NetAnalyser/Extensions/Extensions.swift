a0771bb9f217625cadcd6b3ffe23d86ce73e97ba
Gabriel <gabriel@minucci.com.br>
2020-07-22 09:00:00 -0300

    feat: Improve share functionality

--- Sources/NetAnalyser/Extensions/Extensions.swift
+++ Sources/NetAnalyser/Extensions/Extensions.swift
@@ -11,6 +11,16 @@ extension String {
     var utf8String : UnsafePointer<Int8>? {
         (self as NSString).utf8String
     }
+
+    var jsonPrettyPrinted: String {
+        guard
+            let bodyData = self.data(using: .utf8),
+            let originalEncoded = try? JSONSerialization.jsonObject(with: bodyData, options: .mutableContainers),
+            let prettyEncoded = try? JSONSerialization.data(withJSONObject: originalEncoded, options: .prettyPrinted)
+        else { return self }
+
+        return String(decoding: prettyEncoded, as: UTF8.self)
+    }
 }
 
 extension UnsafePointer where Pointee == UInt8 {
--- Sources/NetAnalyser/Views/Scenes/RequestHistoryDetail/RequestHistoryDetailModels.swift
+++ Sources/NetAnalyser/Views/Scenes/RequestHistoryDetail/RequestHistoryDetailModels.swift
@@ -1,9 +1,8 @@
 //
 //  RequestHistoryDetailModels.swift
-//  Pods
+//
 //
 //  Created by Lucas Paim on 06/05/20.
-//  Copyright (c) 2020 ___ORGANIZATIONNAME___. All rights reserved.
 //
 
 import UIKit
@@ -16,7 +15,30 @@ enum RequestHistoryDetail {
             let history: RequestHistory
         }
         struct ViewModel {
+            let curl: String
+            let uri: String
+            let method: String
+            let server: String
+            let path: String
+            let startTime: String
+            let endTime: String
+            let duration: String
+            let body: String
+            let isUsingSSL: String
+
+            let httpStatus: String
+            let response: String
+            let errorDescription: String
+        }
+    }
+
+    enum Share {
+        struct Request { }
+        struct Response {
             let history: RequestHistory
+        }
+        struct ViewModel {
+            let shareMessage: String
         }
     }
     
